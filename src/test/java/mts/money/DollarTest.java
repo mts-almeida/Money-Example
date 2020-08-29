c6ee839e01453ced39f0495ec53e5424ad3e20a2
mts-almeida <mateusoliveiraalmeida@gmail.com>
2020-08-29 14:22:27 -0300

    adicionando teste de igualdade

--- src/test/java/mts/money/DollarTest.java
+++ src/test/java/mts/money/DollarTest.java
@@ -1,15 +1,23 @@
 package mts.money;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DollarTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void testMultiplication() {
         Dollar five= new Dollar(5);
         Dollar product= five.times(2);
         assertEquals(10, product.amount);
         product= five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testeEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 }

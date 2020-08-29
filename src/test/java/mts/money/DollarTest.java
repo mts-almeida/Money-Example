77d13e0c590aaf1f618472d3a70fbc0179f051f9
mts-almeida <mateusoliveiraalmeida@gmail.com>
2020-08-29 14:05:01 -0300

    novo teste para multiplicação

--- src/test/java/mts/money/DollarTest.java
+++ src/test/java/mts/money/DollarTest.java
@@ -7,7 +7,9 @@ class DollarTest {
     @org.junit.jupiter.api.Test
     public void testMultiplication() {
         Dollar five= new Dollar(5);
-        five.times(2);
+        Dollar product= five.times(2);
         assertEquals(10, five.amount);
+        product= five.times(3);
+        assertEquals(15, five.amount);
     }
 }

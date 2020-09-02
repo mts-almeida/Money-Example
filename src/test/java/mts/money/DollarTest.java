85e53ea3313846bf50d6e5c3bc834887af11699f
mts-almeida <mateusoliveiraalmeida@gmail.com>
2020-09-02 00:38:33 -0300

    criando o factory method Money.dollar

--- src/test/java/mts/money/DollarTest.java
+++ src/test/java/mts/money/DollarTest.java
@@ -2,13 +2,11 @@ package mts.money;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class DollarTest {
 
     @Test
     public void testMultiplication() {
-        Dollar five= new Dollar(5);
+        Money five= Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }

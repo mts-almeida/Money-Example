4ee592d05a67624732af195ab25da672a684135c
mts-almeida <mateusoliveiraalmeida@gmail.com>
2020-09-02 00:48:16 -0300

    usando factory method para remover referencias a classe Dollar

--- src/test/java/mts/money/DollarTest.java
+++ src/test/java/mts/money/DollarTest.java
@@ -9,8 +9,8 @@ class DollarTest {
     @Test
     public void testMultiplication() {
         Money five= Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
 }

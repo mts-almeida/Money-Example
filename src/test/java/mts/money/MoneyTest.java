6002827653f51288e545e7a322359007f5bc335b
mts-almeida <mateusoliveiraalmeida@gmail.com>
2020-09-04 00:26:46 -0300

    criando teste para iqualdade de moeda com classes diferentes

--- src/test/java/mts/money/MoneyTest.java
+++ src/test/java/mts/money/MoneyTest.java
@@ -21,4 +21,9 @@ class MoneyTest {
         assertEquals("CHF",Money.franc(1).currency());
     }
 
+    @Test
+    public void  testDifferentClassEquality(){
+        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+    }
+
 }

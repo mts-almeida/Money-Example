dcacd74a517ba63a9d8958e7aa28579ce36c7261
mts-almeida <mateusoliveiraalmeida@gmail.com>
2020-09-26 11:57:38 -0300

    corrigindo teste

--- src/main/java/mts/money/Money.java
+++ src/main/java/mts/money/Money.java
@@ -23,7 +23,7 @@ public class Money implements Expression{
     }
 
     Expression plus(Money addend){
-        return new Money(amount+ addend.amount, currency);
+        return new Sum(this, addend);
     }
 
     protected String currency() {
--- src/main/java/mts/money/Sum.java
+++ src/main/java/mts/money/Sum.java
@@ -1,6 +1,11 @@
 package mts.money;
 
-public class Sum {
+public class Sum implements Expression {
     Money augend;
     Money addend;
+
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
 }

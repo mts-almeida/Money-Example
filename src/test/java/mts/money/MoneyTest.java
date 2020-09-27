3053fb5277210fbb8f0ea647b6419084e374ed2a
mts-almeida <mateusoliveiraalmeida@gmail.com>
2020-09-27 01:07:18 -0300

    adicionando teste de Sum Plus

--- src/test/java/mts/money/MoneyTest.java
+++ src/test/java/mts/money/MoneyTest.java
@@ -37,4 +37,15 @@ class MoneyTest {
         assertEquals(five, sum.addend);
     }
 
+    @Test
+    public void testSumPlusMoney(){
+        Expression fiveBucks= Money.dollar(5);
+        Expression tenFrancs= Money.franc(10);
+        Bank bank= new Bank();
+        bank.addRate("CHF","USD",2);
+        Expression sum= new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result= bank.reduce(sum,"USD");
+        assertEquals(Money.dollar(15),result);
+    }
+
 }
